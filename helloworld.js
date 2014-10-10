e4e622f8ab44abdd1582fd7abd831bf3ae76f12d
Justin Thareja <justin.thareja@gmail.com>
2014-10-09 21:20:41 -0400

    homework, still on easymode

--- helloworld.js
+++ helloworld.js
@@ -1,6 +1,5 @@
-prompt("What's your name?",name)
 
-console.log("hello from flaky.js");
+/*console.log("hello from flaky.js");
 
 var a = 2;
 var b = 6;
@@ -10,5 +9,142 @@ if (a<b) {
 } else {
 	console.log("else block")
 };
+
+
+//FIZZBUZZ challenge
+//for(initializer;condition;reoccuring statement)
+//initializer = statement that is set before checking the condition
+//condition = statement to be evaluated
+//reoccuring statement = action run after the condition is evaluated
+
+
+for (var i = 1; i <= 100; i++) {
+	if (i % 15 == 0) {
+		console.log("fizzbuzz");
+	} else if (i % 5 == 0) {
+		console.log("buzz");
+	} else if (i % 3 == 0) {
+		console.log("fizz");
+	} else {
+		console.log(i);
+	}
+}
+
+
+for (var counter=100; counter >= 1; counter--) {
+
+    if (counter % 3 == 0 && counter % 5 == 0) {
+        console.log("fizzbuzz");
+    }
+
+    else if (counter % 5 == 0) {
+        console.log("buzz");
+    }
+
+    else if (counter % 3 == 0) {
+        console.log("fizz");
+    }
+
+    else { 
+    	console.log(counter);
+    }
+}
+
+ for (var number = 100; number; number--) {
+
+	if (number % 15 == 0) {
+	    console.log("fizzbuzz");
+	}
+
+	else if (number % 5 == 0) {
+	    console.log("fizz");
+	}
+
+	else if (number % 3 == 0) {
+	    console.log("buzz");
+	}
+
+	else {
+	    console.log(number);
+	}
+}
+
+
+*/
+
+ /* for (var i = 100; i; i--) {
+ 	if (i % 5 == 0) {
+ 		if (i % 3 == 0) {
+ 			console.log("fizzbuzz");
+ 		} 
+
+ 		else {
+			console.log("buzz");
+ 		}	
+ 	if (i % 3 == 0) {
+ 			console.log("fizz");
+ 		}
+	console.log(i); 
+	}
+}
+*/
+
+/*
+var sayHello = function() {
+	var message = "Hello";
+	message = message + "World!";
+	console.log(message);
+}
+
+var x = 1;
+//console.log("x has been set");
+
+x += 10;
+var y = 100;
+//console.log('y has been set');
+//console.log(x has increased)
+
+sayHello();
+sayHello();
+sayHello();
+
+
+[[[[[[[[[[[[[[[[[[[[[[EASY MODE]]]]]]]]]]]]]]]]]]]]]]
+
+
+
+var printer = function() {
+	for (var i = 1; i <= 100; i++)
+		console.log(i);
+}
+
+
+printer();
+
+*/
+
+var number = prompt("Pick a number")
+
+
+var isEven = function() {
+
+}
+
+var debug = function (number) {
+	console.log("Debug" , number)
+}
+
+debug()
+
+
+
+
+
+
+
+
+
+
+
 
 
