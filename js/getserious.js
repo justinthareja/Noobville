f076093536093ef2c4813b0c01ba873228e307ab
Justin Thareja <justin.thareja@gmail.com>
2014-11-07 19:18:25 -0500

    ejs practice problem - lists and arrays

--- js/getserious.js
+++ js/getserious.js
@@ -231,43 +231,64 @@ function reverseArrayInPlace(array) {
 	return array;
 }
 
+// Write a function arrayToList that builds up a data structure like the previous one when given [1, 2, 3] as argument, 
+// and write a listToArray function that produces an array from a list. Also write the helper functions prepend, which 
+// takes an element and a list and creates a new list that adds the element to the front of the input list, and nth, 
+// which takes a list and a number and returns the element at the given position in the list, or undefined when there 
+// is no such element.
 
+var list = {
+  value: 1,
+  rest: {
+    value: 2,
+    rest: {
+      value: 3,
+      rest: null
+    }
+  }
+};
 
+function prepend(element, list) {
+  var newList = {};
+  newList['value'] = element;
+  newList['rest'] = list;
+  return newList;
+}
 
+function nth(list, index) {
+  if (index == 0) 
+    return list.value;
+  else if (index > 0 && list.rest)
+    return nth(list.rest, index - 1);
+  else
+    return undefined;
+}
 
+function arrayToList(array) {
+  var list = {
+    value: array[array.length -1],
+    rest: null
+  };
 
+  for (var i = array.length - 2; i >= 0 ; i--) {
+    list = prepend(array[i], list);
+  }
+  return list;
+}
 
+function listToArray(list) {
+  var array = [];
+  for (var node = list; node; node = node.rest) {
+    array.push(nth(node,0));  
+  }
+  return array;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Write a function, deepEqual, that takes two values and returns true only if they are the same value or are objects with 
+// the same properties whose values are also equal when compared with a recursive call to deepEqual.
+// To find out whether to compare two things by identity (use the === operator for that) or by looking at their properties, 
+// you can use the typeof operator. If it produces "object" for both values, you should do a deep comparison. But you have 
+// to take one silly exception into account: by a historical accident, typeof null also produces "object".
 
 
 
