ec5136805dbd00745419a875e1270eb930ee4b86
Justin Thareja <justin.thareja@gmail.com>
2014-11-13 19:01:09 -0500

    working through ejs ch. 5

--- js/getserious.js
+++ js/getserious.js
@@ -98,31 +98,31 @@ var journal = [
 
 function phi(table) {
   return (table[3] * table[0] - table[2] * table[1]) /
-    Math.sqrt((table[2] + table[3]) *
-              (table[0] + table[1]) *
-              (table[1] + table[3]) *
-              (table[0] + table[2]));
+	Math.sqrt((table[2] + table[3]) *
+			  (table[0] + table[1]) *
+			  (table[1] + table[3]) *
+			  (table[0] + table[2]));
 }
 
-function hasEvent(event, entry) {	
+function hasEvent(event, entry) {   
 	//the "event" argument will be a string that represents one of Jac's daily activities
 	//the "entry" argument will be an index of the journal array
   return entry.events.indexOf(event) != -1; 
-  	//returns a Boolean value. True if the event element can be found within the entry
-  	//indexOf returns a -1 if the element is not present.
+	//returns a Boolean value. True if the event element can be found within the entry
+	//indexOf returns a -1 if the element is not present.
 }
 
 function tableFor(event, journal) {
 	//the "event" argument is the event to be summarized within the journal
 	//the "journal" argument is always the journal array
   var table = [0, 0, 0, 0]; 
-  	//define a blank array with four elements starting at 0
-  	//table format = [no event and no squirrel, yes event and no squirrel, no event and yes squirrel, yes event and yes squirrel]
+	//define a blank array with four elements starting at 0
+	//table format = [no event and no squirrel, yes event and no squirrel, no event and yes squirrel, yes event and yes squirrel]
   for (var i = 0; i < journal.length; i++) { //loops through all journal entries 
-    var entry = journal[i], index = 0; //defines a variable "entry" to be the journal object at index i. defines a variable "index" to 0
-    if (hasEvent(event, entry)) index += 1;  //checks to see if the journal entry at i contains the event argument within any of it's elements. if so, the count has to go up in either table[1] or table[3]
-    if (entry.squirrel) index += 2; //checks to see if the squirrel property within the journal entry is true. if so, the count has to go up in either table[2] or table[3]
-    table[index] += 1; //adds one to the appropriate spot in the table array
+	var entry = journal[i], index = 0; //defines a variable "entry" to be the journal object at index i. defines a variable "index" to 0
+	if (hasEvent(event, entry)) index += 1;  //checks to see if the journal entry at i contains the event argument within any of it's elements. if so, the count has to go up in either table[1] or table[3]
+	if (entry.squirrel) index += 2; //checks to see if the squirrel property within the journal entry is true. if so, the count has to go up in either table[2] or table[3]
+	table[index] += 1; //adds one to the appropriate spot in the table array
   }
   return table;
 }
@@ -144,10 +144,10 @@ function tableFor(event, journal) {
 function gatherCorrelations(journal) {
   var phis = {}; //define empty object
   journal.forEach(function(entry) {
-    entry.events.forEach(function(event) {
-      if (!(event in phis))
-        phis[event] = phi(tableFor(event, journal));
-    });
+	entry.events.forEach(function(event) {
+	  if (!(event in phis))
+		phis[event] = phi(tableFor(event, journal));
+	});
   });
   return phis;
 }
@@ -158,9 +158,9 @@ var correlations = gatherCorrelations(journal);
 
 
 // for (var event in correlations) {
-// 	var correlation = correlations[event];
-// 	if (correlation > 0.1 || correlation < -0.1)
-// 		console.log(event + ": " + correlation); //filter out insignificant correlations
+//  var correlation = correlations[event];
+//  if (correlation > 0.1 || correlation < -0.1)
+//      console.log(event + ": " + correlation); //filter out insignificant correlations
 // }
 
  // Array Methods 
@@ -178,7 +178,7 @@ function urgentlyRememberTo(task) {
 function remove(array, index) {
   console.log(arguments); //arguments is a special variable that stores all arguments that were used when the function is called
   return array.slice(0, index)
-    .concat(array.slice(index + 1));
+	.concat(array.slice(index + 1));
 }
 
 /***********************************************************************************************************
@@ -202,7 +202,7 @@ function range(start, end, step){ //declare a function that takes two arguments 
 	}
 	else {
 		for (i = start; i >= end; i += step)
-			startToEnd.push(i);	
+			startToEnd.push(i); 
 	}
 
 	return startToEnd; //return the array after loop
@@ -214,10 +214,10 @@ function range(start, end, step){ //declare a function that takes two arguments 
 // Run the previous program and see whether it does indeed return 55. 
 
 // function sum(array){ //declare a function that takes an array
-// 	var sum = 0 //define a variable "sum" to keep track of the sum
-// 	for (i = 0; i < array.length; i++) //loop through the whole array using array.length
-// 		sum += array[i]; //add each value to sum
-// 	return sum; //return sum 
+//  var sum = 0 //define a variable "sum" to keep track of the sum
+//  for (i = 0; i < array.length; i++) //loop through the whole array using array.length
+//      sum += array[i]; //add each value to sum
+//  return sum; //return sum 
 // }
 
 
@@ -252,11 +252,11 @@ function reverseArrayInPlace(array) {
 var list = {
   value: 1,
   rest: {
-    value: 2,
-    rest: {
-      value: 3,
-      rest: null
-    }
+	value: 2,
+	rest: {
+	  value: 3,
+	  rest: null
+	}
   }
 };
 
@@ -266,21 +266,21 @@ function prepend(element, list) {
 
 function nth(list, index) {
   if (index == 0) 
-    return list.value;
+	return list.value;
   else if (index > 0 && list.rest)
-    return nth(list.rest, index - 1);
+	return nth(list.rest, index - 1);
   else
-    return undefined;
+	return undefined;
 }
 
 function arrayToList(array) {
   var list = {
-    value: array[array.length -1],
-    rest: null
+	value: array[array.length -1],
+	rest: null
   };
 
   for (var i = array.length - 2; i >= 0 ; i--) {
-    list = prepend(array[i], list);
+	list = prepend(array[i], list);
   }
   return list;
 }
@@ -288,7 +288,7 @@ function arrayToList(array) {
 function listToArray(list) {
   var array = [];
   for (var node = list; node; node = node.rest) {
-    array.push(nth(node,0));  
+	array.push(nth(node,0));  
   }
   return array;
 }
@@ -306,26 +306,26 @@ function deepEqual(x, y) {
   var numPropertiesInX = 0, numPropertiesInY = 0;
 
   if(typeof x == "object" && x != null && 
-     typeof y == "object" && y != null) {
+	 typeof y == "object" && y != null) {
   
-    for (var key in x) 
-      numPropertiesInX++;
-    for (var key in y) 
-      numPropertiesInY++;
+	for (var key in x) 
+	  numPropertiesInX++;
+	for (var key in y) 
+	  numPropertiesInY++;
 
-    if (numPropertiesInX === numPropertiesInY) {
-      for (var key in x) {
-        if (y[key]) 
-          return deepEqual(x[key], y[key]);
-        else 
-          return false;
-      } 
-    }
-    else 
-      return false;
+	if (numPropertiesInX === numPropertiesInY) {
+	  for (var key in x) {
+		if (y[key]) 
+		  return deepEqual(x[key], y[key]);
+		else 
+		  return false;
+	  } 
+	}
+	else 
+	  return false;
   }
   else 
-    return x === y;
+	return x === y;
 }
 
 // // BOOK SOLUTION
@@ -360,7 +360,7 @@ Coderbyte Practice Exercises
  function firstReverse(str) {
   var newString = "";
   for (i = str.length - 1; i >= 0; i--)
-    newString += str[i];
+	newString += str[i];
   return newString;
  }
 
@@ -370,7 +370,7 @@ Coderbyte Practice Exercises
 function firstFactorial(num) {
   var x = num;
   for (i = num - 1; i > 1; i--) 
-    x *= i;
+	x *= i;
   return x;
 }
 
@@ -393,13 +393,13 @@ Treehouse Prototype Intro
 var personPrototype = {
   name: 'Anonymous',
   greet: function (name, mood) {
-      //ensure a valid response if no arguments are given
-      name = name || 'You'; 
-      mood = mood || 'Good';
+	  //ensure a valid response if no arguments are given
+	  name = name || 'You'; 
+	  mood = mood || 'Good';
 
-      console.log('Hello, ' + name +
-                  ' I am ' + this.name +
-                  ' and I am in a ' + mood + ' mood!');
+	  console.log('Hello, ' + name +
+				  ' I am ' + this.name +
+				  ' and I am in a ' + mood + ' mood!');
   },
 
   species: 'Homo Sapien'
@@ -418,10 +418,10 @@ var carPrototype = {
   model: "generic",
   currentGear: 0,
   increaseGear: function() {
-    this.currentGear ++;
+	this.currentGear ++;
   },
   decreaseGear: function() {
-    this.currentGear--;
+	this.currentGear--;
   } 
 }
 
@@ -443,11 +443,12 @@ var array = [1, 2, 3];
 //   console.log(array[i]);
 // }
 
-array.forEach(function (element) {
-  console.log(element);
-});
-
 // refactor using forEach
+
+// array.forEach(function (element) {
+//   console.log(element);
+// });
+
 
 
 
@@ -470,20 +471,20 @@ array.forEach(function (element) {
 function gatherCorrelations (journal) {
   var phis = {};
   journal.forEach(function (entry) {
-    entry.events.forEach(function (event) {
-      if (!(event in phis)) { 
-        phis[event] = phi(tableFor(event, journal)); 
-      }
-    })
+	entry.events.forEach(function (event) {
+	  if (!(event in phis)) { 
+		phis[event] = phi(tableFor(event, journal)); 
+	  }
+	})
   })
   return phis;
 }
-    
+	
 
 // function that creates a new function
 function greaterThan(a) {
   return function(b) {
-    return b > a;
+	return b > a;
   }
 }
 
@@ -491,10 +492,10 @@ function greaterThan(a) {
 // function that changes other functions
 function noisy(f) {
   return function(arg) {
-    console.log("calling with", arg);
-    var val = f(arg);
-    console.log("called with", arg, "- got", val);
-    return val;
+	console.log("calling with", arg);
+	var val = f(arg);
+	console.log("called with", arg, "- got", val);
+	return val;
   };
 }
 
@@ -503,12 +504,12 @@ function noisy(f) {
 
 function unless(test, then) {
   if (!test) {
-    then();
+	then();
   }
 }
 function repeat(times, body) {
   for (var i = 0; i < times; i++) { 
-    body(i);   
+	body(i);   
   }
 }
 
@@ -535,15 +536,107 @@ function repeat(times, body) {
 //     console.log(0, "is even") 
 //   }
 
-var sum = 0;
+var myCar = {
+  make: "Chrystler",
+  model: "Town and Country",
+  year: 1996,
+  color: "White",
+  maxDoorHandles: 5,
+  minDoorHandles: 3,
+  containsStickers: true,
+  inTrunk: "Snow"
+}
 
-repeat(11, function (num) {
-  unless (num <= 10, function () {
-    sum += num;
-  })
+var ANCESTRY_FILE = "[\n  " + [
+  '{"name": "Carolus Haverbeke", "sex": "m", "born": 1832, "died": 1905, "father": "Carel Haverbeke", "mother": "Maria van Brussel"}',
+  '{"name": "Emma de Milliano", "sex": "f", "born": 1876, "died": 1956, "father": "Petrus de Milliano", "mother": "Sophia van Damme"}',
+  '{"name": "Maria de Rycke", "sex": "f", "born": 1683, "died": 1724, "father": "Frederik de Rycke", "mother": "Laurentia van Vlaenderen"}',
+  '{"name": "Jan van Brussel", "sex": "m", "born": 1714, "died": 1748, "father": "Jacobus van Brussel", "mother": "Joanna van Rooten"}',
+  '{"name": "Philibert Haverbeke", "sex": "m", "born": 1907, "died": 1997, "father": "Emile Haverbeke", "mother": "Emma de Milliano"}',
+  '{"name": "Jan Frans van Brussel", "sex": "m", "born": 1761, "died": 1833, "father": "Jacobus Bernardus van Brussel", "mother":null}',
+  '{"name": "Pauwels van Haverbeke", "sex": "m", "born": 1535, "died": 1582, "father": "N. van Haverbeke", "mother":null}',
+  '{"name": "Clara Aernoudts", "sex": "f", "born": 1918, "died": 2012, "father": "Henry Aernoudts", "mother": "Sidonie Coene"}',
+  '{"name": "Emile Haverbeke", "sex": "m", "born": 1877, "died": 1968, "father": "Carolus Haverbeke", "mother": "Maria Sturm"}',
+  '{"name": "Lieven de Causmaecker", "sex": "m", "born": 1696, "died": 1724, "father": "Carel de Causmaecker", "mother": "Joanna Claes"}',
+  '{"name": "Pieter Haverbeke", "sex": "m", "born": 1602, "died": 1642, "father": "Lieven van Haverbeke", "mother":null}',
+  '{"name": "Livina Haverbeke", "sex": "f", "born": 1692, "died": 1743, "father": "Daniel Haverbeke", "mother": "Joanna de Pape"}',
+  '{"name": "Pieter Bernard Haverbeke", "sex": "m", "born": 1695, "died": 1762, "father": "Willem Haverbeke", "mother": "Petronella Wauters"}',
+  '{"name": "Lieven van Haverbeke", "sex": "m", "born": 1570, "died": 1636, "father": "Pauwels van Haverbeke", "mother": "Lievijne Jans"}',
+  '{"name": "Joanna de Causmaecker", "sex": "f", "born": 1762, "died": 1807, "father": "Bernardus de Causmaecker", "mother":null}',
+  '{"name": "Willem Haverbeke", "sex": "m", "born": 1668, "died": 1731, "father": "Lieven Haverbeke", "mother": "Elisabeth Hercke"}',
+  '{"name": "Pieter Antone Haverbeke", "sex": "m", "born": 1753, "died": 1798, "father": "Jan Francies Haverbeke", "mother": "Petronella de Decker"}',
+  '{"name": "Maria van Brussel", "sex": "f", "born": 1801, "died": 1834, "father": "Jan Frans van Brussel", "mother": "Joanna de Causmaecker"}',
+  '{"name": "Angela Haverbeke", "sex": "f", "born": 1728, "died": 1734, "father": "Pieter Bernard Haverbeke", "mother": "Livina de Vrieze"}',
+  '{"name": "Elisabeth Haverbeke", "sex": "f", "born": 1711, "died": 1754, "father": "Jan Haverbeke", "mother": "Maria de Rycke"}',
+  '{"name": "Lievijne Jans", "sex": "f", "born": 1542, "died": 1582, "father":null, "mother":null}',
+  '{"name": "Bernardus de Causmaecker", "sex": "m", "born": 1721, "died": 1789, "father": "Lieven de Causmaecker", "mother": "Livina Haverbeke"}',
+  '{"name": "Jacoba Lammens", "sex": "f", "born": 1699, "died": 1740, "father": "Lieven Lammens", "mother": "Livina de Vrieze"}',
+  '{"name": "Pieter de Decker", "sex": "m", "born": 1705, "died": 1780, "father": "Joos de Decker", "mother": "Petronella van de Steene"}',
+  '{"name": "Joanna de Pape", "sex": "f", "born": 1654, "died": 1723, "father": "Vincent de Pape", "mother": "Petronella Wauters"}',
+  '{"name": "Daniel Haverbeke", "sex": "m", "born": 1652, "died": 1723, "father": "Lieven Haverbeke", "mother": "Elisabeth Hercke"}',
+  '{"name": "Lieven Haverbeke", "sex": "m", "born": 1631, "died": 1676, "father": "Pieter Haverbeke", "mother": "Anna van Hecke"}',
+  '{"name": "Martina de Pape", "sex": "f", "born": 1666, "died": 1727, "father": "Vincent de Pape", "mother": "Petronella Wauters"}',
+  '{"name": "Jan Francies Haverbeke", "sex": "m", "born": 1725, "died": 1779, "father": "Pieter Bernard Haverbeke", "mother": "Livina de Vrieze"}',
+  '{"name": "Maria Haverbeke", "sex": "m", "born": 1905, "died": 1997, "father": "Emile Haverbeke", "mother": "Emma de Milliano"}',
+  '{"name": "Petronella de Decker", "sex": "f", "born": 1731, "died": 1781, "father": "Pieter de Decker", "mother": "Livina Haverbeke"}',
+  '{"name": "Livina Sierens", "sex": "f", "born": 1761, "died": 1826, "father": "Jan Sierens", "mother": "Maria van Waes"}',
+  '{"name": "Laurentia Haverbeke", "sex": "f", "born": 1710, "died": 1786, "father": "Jan Haverbeke", "mother": "Maria de Rycke"}',
+  '{"name": "Carel Haverbeke", "sex": "m", "born": 1796, "died": 1837, "father": "Pieter Antone Haverbeke", "mother": "Livina Sierens"}',
+  '{"name": "Elisabeth Hercke", "sex": "f", "born": 1632, "died": 1674, "father": "Willem Hercke", "mother": "Margriet de Brabander"}',
+  '{"name": "Jan Haverbeke", "sex": "m", "born": 1671, "died": 1731, "father": "Lieven Haverbeke", "mother": "Elisabeth Hercke"}',
+  '{"name": "Anna van Hecke", "sex": "f", "born": 1607, "died": 1670, "father": "Paschasius van Hecke", "mother": "Martijntken Beelaert"}',
+  '{"name": "Maria Sturm", "sex": "f", "born": 1835, "died": 1917, "father": "Charles Sturm", "mother": "Seraphina Spelier"}',
+  '{"name": "Jacobus Bernardus van Brussel", "sex": "m", "born": 1736, "died": 1809, "father": "Jan van Brussel", "mother": "Elisabeth Haverbeke"}'
+].join(",\n  ") + "\n]";
+
+
+// parse the JSON ancestry file into Javascript Object format
+var ancestry = JSON.parse(ANCESTRY_FILE); 
+
+// filter the ancestry list for all females
+var femaleAncestry = ancestry.filter(function (person) {
+  return person.sex === "f"
+});
+
+// map all female names in the ancestry data set
+var femaleNames = femaleAncestry.map(function (female) {
+	return female.name;
 })
 
-console.log(sum);
+// use reduce to find the oldest known ancestor
+var oldestKnown = ancestry.reduce(function (min, cur) {
+	return cur.born < min.born ? cur : min;
+})
+
+
+function average (array) {
+	function plus (a, b) {
+		return a + b;
+	}
+	return array.reduce(plus) / array.length
+}
+
+function age(p) { return p.died - p.born; }
+function male(p) { return p.sex === 'm'; }
+function female(p) { return p.sex === 'f';}
+
+// find the average age of males in the ancestry
+
+// var maleAncestry = ancestry.filter(male);
+// var maleAge = maleAncestry.map(age);
+// var avgMaleAge = average(maleAge);
+
+var avgMaleAge = average(ancestry.filter(male).map(age));
+var avgFemaleAge = average(ancestry.filter(female).map(age));
+
+console.log(avgMaleAge);
+console.log(avgFemaleAge);
+
+var byName = {};
+ancestry.forEach(function (person) {
+	byName[person.name] = person;
+})
+
 
 
 
